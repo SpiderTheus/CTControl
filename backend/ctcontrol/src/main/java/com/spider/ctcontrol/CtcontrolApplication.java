a9f900a5b6a023d19260de81765a5849bc0bacef
spider <matemora80@gmail.com>
2025-10-03 21:41:27 -0300

    Initial setup

---
+++ backend/ctcontrol/src/main/java/com/spider/ctcontrol/CtcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.spider.ctcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtcontrolApplication.class, args);
+	}
+
+}
