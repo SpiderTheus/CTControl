b51e7f2a042a1a6b583973ffd248ab46b0d3b83c
spider <matemora80@gmail.com>
2025-12-17 10:05:14 -0300

    fix: ignore secret key and clean repository

--- backend/ctcontrol/src/main/java/com/spider/ctcontrol/repositories/UserRepository.java
+++ backend/ctcontrol/src/main/java/com/spider/ctcontrol/repositories/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spider.ctcontrol.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     
     Optional<User> findByUsername(String username);
 }
